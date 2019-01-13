8d0eeda73184d15303bb66fb7d7420391a4912c9
KennedyCzar <kennedyczar@gmail.com>
2019-01-13 22:25:07 +0100

    Update KMENAS_MNIST.py

--- UNSUPERVISED LEARNING/UNSUPERVISED I/KMENAS_MNIST.py
+++ UNSUPERVISED LEARNING/UNSUPERVISED I/KMENAS_MNIST.py
@@ -1,16 +1,16 @@
-# https://deeplearningcourses.com/c/cluster-analysis-unsupervised-machine-learning-python
-# https://www.udemy.com/cluster-analysis-unsupervised-machine-learning-python
-
 # data is from https://www.kaggle.com/c/digit-recognizer
 # each image is a D = 28x28 = 784 dimensional vector
 # there are N = 42000 samples
 # you can plot an image by reshaping to (28,28) and using plt.imshow()
-
 from __future__ import print_function, division
 from future.utils import iteritems
 from builtins import range, input
 # Note: you may need to update your version of future
 # sudo pip install -U future
+
+__author__ = "kennedy Czar"
+__email__ = "kennedyczar@gmail.com"
+__version__ = '1.0'
 
 
 import numpy as np
